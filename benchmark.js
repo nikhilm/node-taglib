96a7d506b7a19745d8a961997f44e78c33b6fdd5
Nikhil Marathe <nsm.nikhil@gmail.com>
2012-02-27 23:11:18 +0530

    Committed buggy code.
    This directly creates FileRef rather than using CreateFileRef.
    You can see that on multiple runs, it will give deallocation errors

--- benchmark.js
+++ benchmark.js
@@ -1,27 +1,25 @@
 var taglib = require('./build/Release/taglib');
 var async = require('async');
+var fs = require('fs');
 var match = require('match-files');
-
-var args = process.argv.slice(2);
 
 var isMp3 = function(path) { return path.match(/\.mp3$/); }
 
 match.find('/Users/nikhilmarathe/Music', {fileFilters: [isMp3]}, function(err, files) {
+    var count = 0;
+    var fEC = 0;
     var t = Date.now();
     async.forEach(files, function(fn, cb) {
-        try {
-            taglib.tag(fn, function() {});
-        } catch(e) {}
-        cb(false);
+        fEC++;
+        //console.log("forEach", fEC);
+        taglib.tag(fn, function(err, tag) {
+            count++;
+            console.log(tag.title);
+            //tag.dispose();
+            cb(false);
+        });
     }, function() {
         console.log("Async", Date.now() - t);
+        console.log("Tag succeeded on ", count);
     });
-
-    t = Date.now();
-    files.forEach(function(elem) {
-        try {
-            taglib.tagSync(elem);
-        } catch(e) {}
-    });
-    console.log("Sync", Date.now() - t);
 });
