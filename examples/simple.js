1f0d16378be9a15c3720b00c17ab8f928fe3e20b
Nikhil Marathe <nsm.nikhil@gmail.com>
2012-03-22 08:42:45 +0530

    update example to use async API

--- examples/simple.js
+++ examples/simple.js
@@ -2,6 +2,7 @@ var Taglib = require(__dirname+"/../taglib")
 var util = require("util")
 
 for (var i = 2; i < process.argv.length; i++) {
-    var p = new Taglib.Tag(process.argv[i]);
-    util.debug( process.argv[i] + ": " + util.inspect(p) )
+    Taglib.tag(process.argv[i], function(err, tag) {
+        console.dir(err ? err : tag);
+    });
 }
