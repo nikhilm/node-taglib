bb366bd5f1e6543e8817120ddd4c5567c4e75b1a
Nikhil Marathe <nsm.nikhil@gmail.com>
2012-05-29 13:57:42 +0530

    Don't run tests in isolation

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 make || exit 1
 npm link vows
-./node_modules/vows/bin/vows --spec --isolate spec/taglibSpec.js spec/resolverSpec.js spec/buffersSpec.js
+./node_modules/vows/bin/vows --spec spec/taglibSpec.js spec/resolverSpec.js spec/buffersSpec.js
