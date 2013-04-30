3e2fbec0201f857dda0ecac725998d12c404e9c6
Nikhil Marathe <nsm.nikhil@gmail.com>
2013-04-30 20:47:18 +0530

    Update examples

--- examples/properties.js
+++
@@ -1,5 +0,0 @@
-var Taglib = require('bindings')('taglib');
-var util = require("util")
-
-var p = new Taglib.AudioProperties(process.argv[2]);
-util.debug( util.inspect(p) )
--- examples/simple.js
+++ examples/simple.js
@@ -2,7 +2,7 @@ var Taglib = require('bindings')('taglib');
 var util = require("util")
 
 for (var i = 2; i < process.argv.length; i++) {
-    Taglib.read(process.argv[i], function(err, tag, props) {
-        console.dir(err ? err : {'tag': tag, 'audioProperties': props});
+    Taglib.read(process.argv[i], function(err, m) {
+        console.dir(err ? err : m);
     });
 }
